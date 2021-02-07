bf69efc8db288315b6eb295a0dcd1cd2de0e5987
caiheen <gkdms0520@gmail.com>
2021-02-07 19:23:42 +0900

    Auto typing

---
+++ autotyping/main.js
@@ -0,0 +1,30 @@
+const span = document.querySelector(".txt-wrap"); // span을 html에서 불러온다
+const words = JSON.parse(span.getAttribute("data-words"));
+// span 안에 들어있는 data-words 배열을 JSON.parse 배열로 가져온다
+let txt = ""; // span 안에 찍을 글자
+let index = 0; // 배열의 인덱스
+let isDeleting = false; // false면 입력 중!
+
+function init(){
+    const current = index % words.length; // 모듈로 연산
+    
+    if(isDeleting){ // 입력이 완료 되었다면 
+        txt = "";
+    } else { // 아직 입력 중이라면
+        txt = words[current].substring(0, txt.length + 1); //하나씩 추가
+    }
+
+    span.innerHTML = `<span>${txt}</span>` // span에 txt를 넣어준다.
+
+    if(txt === words[current] && !isDeleting){ 
+        isDeleting = true;
+    } else if(txt == "" && isDeleting){
+        isDeleting = false;
+        index++;
+    }
+
+    setTimeout(init, 300);
+}
+
+init();
+

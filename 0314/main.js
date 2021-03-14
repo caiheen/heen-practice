b7882244d082f523ae3dd94e67551214e3458cba
caiheen <gkdms0520@gmail.com>
2021-03-14 21:38:07 +0900

    사칙연산

--- 0314/main.js
+++ 0314/main.js
@@ -1,6 +1,28 @@
-const txt = document.querySelector("p");
+const ul = document.querySelector("ul");
+// A+B, A-B, A*B, A/B(몫), A%B(나머지)
 const A = prompt("A한 자리수 정수를 입력하세요.", "");
 const B = prompt("B숫자를 입력해주세요");
 
-let C = Number(A) / Number(B);
-txt.innerHTML = C;
+function results(){
+    let C = Number(A) + Number(B);
+    let D = Number(A) - Number(B);
+    let E = Number(A) * Number(B);
+    let F = Number(A) / Number(B);
+    let G = Number(A) % Number(B);
+    const add = document.createElement("li");
+    const minus = document.createElement("li");
+    const multi = document.createElement("li");
+    const division = document.createElement("li");
+    const percent = document.createElement("li");
+    ul.appendChild(add);
+    ul.appendChild(minus);
+    ul.appendChild(multi);
+    ul.appendChild(division);
+    ul.appendChild(percent);
+    add.innerHTML = C;
+    minus.innerHTML = D;
+    multi.innerHTML = E;
+    division.innerHTML = F;
+    percent.innerHTML = G;
+}
+results();

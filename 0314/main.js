34337d2121addf25530a475572344cb25966d5a2
caiheen <gkdms0520@gmail.com>
2021-03-14 20:49:24 +0900

    A*B

--- 0314/main.js
+++ 0314/main.js
@@ -2,5 +2,5 @@ const txt = document.querySelector("p");
 const A = prompt("A한 자리수 정수를 입력하세요.", "");
 const B = prompt("B숫자를 입력해주세요");
 
-let C = Number(A) - Number(B);
+let C = Number(A) * Number(B);
 txt.innerHTML = C;

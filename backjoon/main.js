ddecb5b01990bb560c309d4ee5fc9178fad4ac29
caiheen <gkdms0520@gmail.com>
2021-03-19 22:40:56 +0900

    두수 비교하기

--- backjoon/main.js
+++ backjoon/main.js
@@ -1,5 +1,13 @@
-document.write("/|    /|<br/>");
-document.write(" )  ( ')<br/>");
-document.write("(  /  )<br/>");
-document.write("|(__)|<br/>");
+let A = prompt("A에 들어갈 숫자를 입력하세요!");
+let B = prompt("B에 들어갈 숫자를 입력하세요!");
 
+function results(){
+    if(Number(A) > Number(B)){
+        document.write(">");
+    } else if(Number(A) < Number(B)){
+        document.write("<");
+    } else if(Number(A) == Number(B)){
+        document.write("==");
+    }
+}
+results();

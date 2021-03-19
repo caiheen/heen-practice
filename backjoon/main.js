2d7fa3e0352d96c39cc3b6724292060c16aee313
caiheen <gkdms0520@gmail.com>
2021-03-19 22:50:06 +0900

    시험성적

--- backjoon/main.js
+++ backjoon/main.js
@@ -1,13 +1,16 @@
-let A = prompt("A에 들어갈 숫자를 입력하세요!");
-let B = prompt("B에 들어갈 숫자를 입력하세요!");
+let A = prompt("시험 점수를 입력해주세요!");
 
 function results(){
-    if(Number(A) > Number(B)){
-        document.write(">");
-    } else if(Number(A) < Number(B)){
-        document.write("<");
-    } else if(Number(A) == Number(B)){
-        document.write("==");
+    if(Number(A) >= 90 && Number(A) <= 100){
+        document.write("A");
+    } else if(Number(A) >= 80 && Number(A) < 89){
+        document.write("B");
+    } else if(Number(A) >= 70 && Number(A) < 79){
+        document.write("C");
+    } else if(Number(A) >= 60 && Number(A) < 69){
+        document.write("D");
+    } else{
+        document.write("F");
     }
 }
 results();

9745ea92a5d758b026bd9aeedf827326b2449063
caiheen <gkdms0520@gmail.com>
2021-03-19 23:39:56 +0900

    구구단

--- backjoon/main.js
+++ backjoon/main.js
@@ -1,16 +1,9 @@
-let H = prompt("시를 입력해주세요!");
-let M = prompt("분을 입력해주세요!");
+let N = prompt("몇단?");
 
-
-function results(){
-    M -= 45;
-    if(M < 0){
-        M += 60;
-        H--;
-        if(H === -1){
-            H = 23;
-        }
-    }
+document.write(Number(N) + "단");
+document.write('<br/>');
+for(let i = 1; i < 10; i++){
+    document.write(Number(N) + '*' + i + "=" + (Number(N) * i));
+    document.write('<br/>');
 }
-results();
-document.write(H,":",M)
+

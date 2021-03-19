5102d98c7124c23392c3827e4f9e1c9c16b41fb0
caiheen <gkdms0520@gmail.com>
2021-03-19 22:05:33 +0900

    backjoon_0319

--- backjoon/main.js
+++ backjoon/main.js
@@ -1,28 +1,27 @@
 const ul = document.querySelector("ul");
 // A+B, A-B, A*B, A/B(몫), A%B(나머지)
-const A = prompt("A한 자리수 정수를 입력하세요.", "");
-const B = prompt("B숫자를 입력해주세요");
+const A = prompt("A에 들어갈 숫자를 입력하세요.");
+const B = prompt("B에 들어갈 숫자를 입력하세요.");
+const C = prompt("C에 들어갈 숫자를 입력하세요.")
 
 function results(){
-    let C = Number(A) + Number(B);
-    let D = Number(A) - Number(B);
-    let E = Number(A) * Number(B);
-    let F = Number(A) / Number(B);
-    let G = Number(A) % Number(B);
-    const add = document.createElement("li");
+    let D = (Number(A) + Number(B)) % Number(C);
+    let E = (Number(A) % Number(C)) + (Number(B) % Number(C)) % Number(C);
+    let F = (Number(A) * Number(B)) % Number(C);
+    let G = (Number(A) % Number(C)) * (Number(B) % Number(C)) % Number(C);
     const minus = document.createElement("li");
     const multi = document.createElement("li");
     const division = document.createElement("li");
     const percent = document.createElement("li");
-    ul.appendChild(add);
     ul.appendChild(minus);
     ul.appendChild(multi);
     ul.appendChild(division);
     ul.appendChild(percent);
-    add.innerHTML = C;
     minus.innerHTML = D;
     multi.innerHTML = E;
     division.innerHTML = F;
     percent.innerHTML = G;
+
+    console.log(D);
 }
 results();
